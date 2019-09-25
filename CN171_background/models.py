ca5f262bd497eb5289fd86d3efe8307d4d2c68cd
xxxjkk <xxxjkk@126.com>
2019-09-25 21:44:28 +0800

    新增后台数据库数据显示

--- CN171_background/models.py
+++ CN171_background/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BgTaskManagement(models.Model):
+    # '''后台管理表'''
+    bg_id = models.AutoField(primary_key=True)
+    bg_module = models.CharField(max_length=56)
+    bg_domain = models.CharField(max_length=56)
+    bg_status = models.CharField(max_length=32,null=True, blank=True)
+    bg_lastopr_user = models.CharField(max_length=56,null=True, blank=True)
+    bg_lastopr_type = models.CharField(max_length=32,null=True, blank=True)
+    bg_lastopr_time = models.DateTimeField(auto_now_add=True,null=True, blank=True)
+    bg_lastopr_result = models.CharField(max_length=32,null=True, blank=True)
+    bg_task_start = models.TextField()
+    bg_task_stop = models.TextField()
+    bg_task_restart = models.TextField()
+    bg_task_query = models.TextField()
+    bg_insert_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.dis_name
+
+    class Meta:
+        verbose_name = u'后台管理表'
+        verbose_name_plural = verbose_name
+        db_table = "bg_task_management"
+
+
+class BgTaskLog(models.Model):
+    # '''后台管理日志表'''
+    bg_log_id = models.AutoField(primary_key=True)
+    bg_id = models.IntegerField(max_length=56)
+    bg_operation_user = models.CharField(max_length=56,null=True, blank=True)
+    bg_operation = models.CharField(max_length=32,null=True, blank=True)
+    bg_opr_result = models.CharField(max_length=32,null=True, blank=True)
+    bg_operation_time = models.DateTimeField(auto_now_add=True)
+    bg_log_dir = models.CharField(max_length=32,null=True, blank=True)
+
+    def __unicode__(self):
+        return self.dis_name
+
+    class Meta:
+        verbose_name = u'后台管理日志表'
+        verbose_name_plural = verbose_name
+        db_table = "bg_task_log"
