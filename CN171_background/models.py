7a9274a6effb857a4760d239e53093e9aaaf3340
bai0718yang <baiyangby.66@163.com>
2019-09-30 16:29:40 +0800

    '更新查询按钮'

--- CN171_background/models.py
+++ CN171_background/models.py
@@ -5,19 +5,19 @@ from django.db import models
 
 class BgTaskManagement(models.Model):
     # '''后台管理表'''
-    bg_id = models.AutoField(primary_key=True)
-    bg_module = models.CharField(max_length=56)
-    bg_domain = models.CharField(max_length=56)
-    bg_status = models.CharField(max_length=32,null=True, blank=True)
-    bg_lastopr_user = models.CharField(max_length=56,null=True, blank=True)
-    bg_lastopr_type = models.CharField(max_length=32,null=True, blank=True)
-    bg_lastopr_time = models.DateTimeField(auto_now_add=True,null=True, blank=True)
-    bg_lastopr_result = models.CharField(max_length=32,null=True, blank=True)
-    bg_task_start = models.TextField()
-    bg_task_stop = models.TextField()
-    bg_task_restart = models.TextField()
-    bg_task_query = models.TextField()
-    bg_insert_time = models.DateTimeField(auto_now_add=True)
+    bg_id = models.AutoField(primary_key=True, verbose_name=u"任务管理id")
+    bg_module = models.CharField(max_length=56, verbose_name=u"归属模块")
+    bg_domain = models.CharField(max_length=56, verbose_name=u"归属中心")
+    bg_status = models.CharField(max_length=32, verbose_name=u"状态", null=True, blank=True)
+    bg_lastopr_user = models.CharField(max_length=56, verbose_name=u"最后一次操作人员", null=True, blank=True)
+    bg_lastopr_type = models.CharField(max_length=32, verbose_name=u"最后一次操作类型", null=True, blank=True)
+    bg_lastopr_time = models.DateTimeField(auto_now_add=True,verbose_name=u"最后一次操作时间", null=True, blank=True)
+    bg_lastopr_result = models.CharField(max_length=32,verbose_name=u"操作结果", null=True, blank=True)
+    bg_task_start = models.TextField(verbose_name=u"任务启动脚本")
+    bg_task_stop = models.TextField(verbose_name=u"任务暂停脚本")
+    bg_task_restart = models.TextField(verbose_name=u"任务重启脚本")
+    bg_task_query = models.TextField(verbose_name=u"任务查询脚本")
+    bg_insert_time = models.DateTimeField(auto_now_add=True, verbose_name=u"插入时间")
 
     def __unicode__(self):
         return self.dis_name
--- CN171_background/views.py
+++ CN171_background/views.py
@@ -5,7 +5,7 @@ from CN171_background.api import pages
 from django.shortcuts import render,redirect
 from CN171_background.models import BgTaskManagement
 from CN171_tools import connecttool
-
+from django.db.models import Q
 
 # Create your views here.
 
@@ -13,7 +13,19 @@ from CN171_tools import connecttool
 def taskManagement(request):
     # 获取所有后台对象
     # page_len = request.GET.get('page_len', '')
-    task_list = models.BgTaskManagement.objects.all()
+    task_list=[]
+    keyword = request.GET.get("keyword","")
+    if keyword:
+        task_list=models.BgTaskManagement.objects.filter(
+            Q(bg_module__icontains = keyword ) |
+            Q(bg_domain__icontains = keyword ) |
+            Q(bg_status__icontains = keyword)  |
+            Q(bg_lastopr_user__icontains = keyword) |
+            Q(bg_lastopr_type__icontains =keyword)  |
+            Q(bg_lastopr_result__icontains= keyword)
+        )
+    else:
+        task_list = models.BgTaskManagement.objects.all()
     p, page_objects, page_range, current_page, show_first, show_end, end_page, page_len = pages(task_list, request)
     return render(request, "background/task_management.html", locals())
 
