1b271214cbee1aaa1102cdcd71e7ecde23aca776
xxxjkk <xxxjkk@126.com>
2019-10-24 17:44:55 +0800

    新增crontab APP

---
+++ CN171_crontab/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  : 2019/10/17 16:10
+# @Author: zhulong
+# @File  : urls.py
+# @Software: CN171
+
+from django.conf.urls import url
+from CN171_crontab import views as crontabviews
+
+app_name = "CN171_crontab"
+
+urlpatterns = [
+    url(r'^add/', crontabviews.addaction, name='add'),
+]
