5dff2e1946be5edbc3eeda2b9e3b6cc1c1d62ca3
xxxjkk <xxxjkk@126.com>
2019-11-19 04:46:24 +0800

    新增配置管理功能

--- CN171_config/urls.py
+++ CN171_config/urls.py
@@ -5,4 +5,6 @@ from CN171_config import views as configViews
 urlpatterns = [
 
     url(r'^configmanagement/', configViews.configManagement, name='configManagement'),
+    url(r'^configsave/', configViews.configSave, name='configsave'),
+    url(r'^restartcn171/', configViews.reStartCN171, name='restartcn171'),
 ]
--- CN171_config/views.py
+++ CN171_config/views.py
@@ -1,5 +1,6 @@
 import os
 from django.shortcuts import render
+from django.http import JsonResponse
 try:
     import ConfigParser as cp
 except ImportError as e:
@@ -8,14 +9,59 @@ except ImportError as e:
 #读取配置文件
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 config = cp.ConfigParser()
-config.read(os.path.join(BASE_DIR, 'config/cn171.conf'),encoding='utf-8')
+config.read(os.path.join(BASE_DIR, 'config/cn171.conf'), encoding='utf-8')
 
 
+#配置文件页面主函数
 def configManagement(request):
-    #获取配置文件中的sections
-    print(config.sections())
+    #获取配置文件中的sections，一维数组
     sectionlist = config.sections()
-    print(config.items(sectionlist[0]))
-    items = config.items(sectionlist[0])
+
+    #获取配置文件中的option和item，三维数组
+    itemlist3 = []
+    for section in sectionlist:
+        items = config.items(section)
+        itemlist3.append(items)
 
     return render(request,"config/config_management.html",locals())
+
+#修改配置文件函数
+def configSave(request):
+    #获取界面传入的配置数据
+    list_section = request.POST.getlist('list_section')
+    list_value_str = request.POST.getlist('list_value')
+    list_key_str = request.POST.getlist('list_key')
+    list_value = []
+    list_key = []
+    for i in range(len(list_value_str)):
+        list_value.append(list_value_str[i].split(','))
+        list_key.append(list_key_str[i].split(','))
+
+    #获取配置文件中的配置数据
+    sectionlist = config.sections()
+    keylist = []
+    valuelist = []
+    for section in sectionlist:
+        options = config.options(section)
+        keylist.append(options)
+
+        templist = []
+        for i in range(len(options)):
+            templist.append(config.get(section,options[i]))
+        valuelist.append(templist)
+
+    #配置文件比对，存在差异的配置项，以前台传入的为主进行保存
+    for i in range(len(sectionlist)):
+        for j in range(len(keylist[i])):
+            if valuelist[i][j] != list_value[i][j]:
+                config.set(sectionlist[i], keylist[i][j], list_value[i][j])
+    config.write(open(os.path.join(BASE_DIR, 'config/cn171.conf'), "w"))
+
+    return JsonResponse({'executeflag': "成功"})
+
+
+#修改配置文件函数
+def reStartCN171(request):
+    os.system(BASE_DIR + "/cn171.sh restart")
+
+    return JsonResponse({'executeflag': "成功"})
