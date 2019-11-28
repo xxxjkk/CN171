d4a860d288713152428ce35a70fd8729710830f8
yangchunzhao <1129166939@qq.com>
2019-11-28 15:36:56 +0800

    更新用户管理模块（用户管理、权限管理）

--- CN171_account/urls.py
+++ CN171_account/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.conf.urls import include,url
 from CN171_account import views as userViews
 urlpatterns = [
-
    url(r'^userManagement/', userViews.userManagement, name='userManagement'),
    url(r'^userAdd/', userViews.userAdd, name='userAdd'),
    url(r'^userDel/', userViews.userDel, name='userDel'),
-   url(r'^userEdit/', userViews.userEdit, name='userEdit'),
+   url(r'^userEdit/(?P<acc_user_id>\d+)/$', userViews.userEdit, name='userEdit'),
+   url(r'^userStatusEdit/', userViews.userStatusEdit, name='userStatusEdit'),
+   url(r'^userSearch/', userViews.userSearch, name='userSearch'),
+   url(r'^distribute_permissions/', userViews.distribute_permissions, name='distribute_permissions'),
 ]
--- CN171_login/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Cn171LoginConfig(AppConfig):
-    name = 'CN171_login'
