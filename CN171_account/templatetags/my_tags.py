c04bc151e5078809e418b5c8ca28eac8d7ce6cdd
yangchunzhao <1129166939@qq.com>
2019-12-04 17:34:51 +0800

    修复菜单权重BUG

--- CN171_account/templatetags/my_tags.py
+++ CN171_account/templatetags/my_tags.py
@@ -5,22 +5,34 @@
 # @File  : my_tags.py.py
 # @Software: CN171
 import re
+from collections import OrderedDict
 
 from django import template
 register = template.Library()
 @register.inclusion_tag("main-sidebar.html")
 def get_menu_styles(request):
     permission_menu_dict = request.session.get("permission_menu_dict")
-    print("permission_menu_dict", permission_menu_dict)
 
-    for val in permission_menu_dict.values():
-        for item in val["children"]:
-            val["class"] = "hide"
-            ret = re.search("^{}$".format(item["url"]), request.path)
+    # 因为字典是无序的，要使菜单显示有序:
+    # 1.在model的菜单表中设置weight权重字段，
+    # 2.引用sorted()按权重排序倒叙，权重越大显示越靠前
+    # 3.将数据放到有序字典中
+    # sorted() 函数对所有可迭代的对象进行排序操作。
+    # sort 是应用在 list 上的方法，sorted 可以对所有可迭代的对象进行排序操作。
+
+    order_dict = OrderedDict()
+    for i in sorted(permission_menu_dict, key=lambda x: permission_menu_dict[x]['menu_weight'], reverse=True):
+        # 复制到order_dict中
+        order_dict[i] = permission_menu_dict[i]
+        # 取一级菜单的信息
+        item = order_dict[i]
+        for j in item["children"]:
+            item["class"] = "hide"
+            ret = re.search("^{}$".format(j["url"]), request.path)
             if ret:
-                val["class"] = ""
-
-    return {"permission_menu_dict": permission_menu_dict}
+                item["class"] = ""
+    print(order_dict.values())
+    return {"permission_menu_dict": order_dict}
 
 @register.filter
 def has_permission(btn_url, request):
